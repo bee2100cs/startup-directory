74f519052f5f68f8942d44a5d7c2870cb5453444
Bryan Muchai <bee2100cs@gmail.com>
2025-02-28 08:44:49 +0300

    Implement server side rendered search form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
             colors: {
                 primary: {
                     "100": "#FFE8F0",
-                    DEFAULT: "#EE2B69",
+                    DEFAULT: "#7091E6",
                 },
                 secondary: "#FBE843",
                 black: {
